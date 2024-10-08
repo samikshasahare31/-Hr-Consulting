27df62d6ea932113c9c2361cc04d00c4be01c5da
Mohammed Umar khan <mohammedumarkhan803@gmail.com>
2024-10-08 13:03:43 +0530

    resource page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import Resources from "./components/resources/Resources";
 import Navbar from "./components/Navbar";
 // import Home from "./components/home/Home";
 import PeoEor from "./components/services/peo&eor/PeoEor";
