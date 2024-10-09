e691af7c6a32988054fbfd10c4029812ed6c13d0
Vikas24Chauhan <vikas24chauhan@gmail.com>
2024-10-09 12:01:06 +0530

    Services PEO solutions and benefits content is changed

--- src/assets/data/peoBenefit.js
+++ src/assets/data/peoBenefit.js
@@ -1,31 +1,37 @@
+import proBenefit1 from "../images/proBenefit1.PNG";
+import proBenefit2 from "../images/proBenefit2.WEBP";
+import proBenefit3 from "../images/proBenefit3.JPG";
+import proBenefit4 from "../images/proBenefit4.JPG";
+import proBenefit5 from "../images/proBenefit5.JPG";
+
 export const peoBenefits = [
   {
     id: 1,
-    image: "https://recruitindo.com/wp-content/uploads/2023/12/save-time.jpeg",
+    image: proBenefit1,
     name: "Optimize Your Schedule",
     desc: "Prioritize the enhancement of your product offerings, strategic planning, and building lasting connections with clients and partners. By simplifying the complexities of paperwork, benefits management, payroll processing, and contract termination, you free up your schedule to focus on driving your business forward.",
   },
   {
     id: 2,
-    image: "https://recruitindo.com/wp-content/uploads/2023/12/save-time.jpeg",
+    image: proBenefit2,
     name: "Optimize Spending",
     desc: "Establish your presence in Indonesia easily and affordably. Our comprehensive services cover every aspect of setting up a subsidiary, from legal to financial, ensuring you save time and avoid the high costs of beginning from scratch in a foreign market.",
   },
   {
     id: 3,
-    image: "https://recruitindo.com/wp-content/uploads/2023/12/save-time.jpeg",
+    image: proBenefit3,
     name: "Limit Potential Risks",
     desc: "Delegate the task of managing tax documentation and local labor law compliance to us. This shift allows your organization to concentrate on its essential functions without the burden of regulatory or legal concerns. Rely on our expertise to mitigate potential risks and ensure smooth operations.",
   },
   {
     id: 4,
-    image: "https://recruitindo.com/wp-content/uploads/2023/12/save-time.jpeg",
+    image: proBenefit4,
     name: "Mitigate Barriers to Entry",
     desc: "Empower your business growth in Indonesia with our knowledgeable support. We’ll guide you through the intricacies of legal, regulatory, and administrative processes without hassle.",
   },
   {
     id: 5,
-    image: "https://recruitindo.com/wp-content/uploads/2023/12/save-time.jpeg",
+    image: proBenefit5,
     name: "Simplified Compliance Process",
     desc: "Trust our services to deliver local compliance and effective safeguards, ensuring the safety of your proprietary information.",
   },
--- src/components/services/peo&eor/PeoEor.jsx
+++ src/components/services/peo&eor/PeoEor.jsx
@@ -1,65 +1,82 @@
 import React from "react";
 import "./PeoEor.css";
+import peoMainImage from "../../../assets/images/peo-main-image.JPG";
 import { peoSolutions } from "../../../assets/data/peoSolution";
 import { peoBenefits } from "../../../assets/data/peoBenefit";
 import { peoPricing } from "../../../assets/data/peoPricing";
 import rightArrow from "../../../assets/images/right-arrow.svg";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 
 function PeoEor() {
-  const { t } = useTranslation(); 
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const handleclick = () => {
+    navigate("/contactUs");
+  };
+
   const peoResources = [
     {
       id: 0,
       img: "https://recruitindo.com/wp-content/uploads/2023/12/eliminate.png",
       article: t("Articles"),
       resources: t("Resources"),
-      title:
-       t("The Ultimate Guide to Executive Search in Indonesia: Analysing the Financial and Cultural Impact of Wrong Hires"),
-      text: t("Introduction In today’s competitive business landscape, finding and retaining top executive talent is crucial."),
+      title: t(
+        "The Ultimate Guide to Executive Search in Indonesia: Analysing the Financial and Cultural Impact of Wrong Hires"
+      ),
+      text: t(
+        "Introduction In today’s competitive business landscape, finding and retaining top executive talent is crucial."
+      ),
     },
     {
       id: 1,
       img: "https://recruitindo.com/wp-content/uploads/2023/12/recruitindo-by-number-1024x682.jpeg",
       article: t("Articles"),
       resources: t("Resources"),
-      title:
-        t("Managing Remote Teams in Indonesia: Essential Tools and Strategies via PEO/EOR Services"),
-      text: t("Introduction In the age of globalisation and digital transformation, the ability to manage remote teams effectively"),
+      title: t(
+        "Managing Remote Teams in Indonesia: Essential Tools and Strategies via PEO/EOR Services"
+      ),
+      text: t(
+        "Introduction In the age of globalisation and digital transformation, the ability to manage remote teams effectively"
+      ),
     },
     {
       id: 2,
       img: "https://recruitindo.com/wp-content/uploads/2024/01/search-solution-1024x768.jpeg",
       article: t("Articles"),
       resources: t("Resources"),
-      title:
-        t("Strategic Executive Search Solutions for Indonesia’s Top Businesses"),
-      text: t("In today’s fiercely competitive business landscape, finding the right talent to lead your organisation is more"),
+      title: t(
+        "Strategic Executive Search Solutions for Indonesia’s Top Businesses"
+      ),
+      text: t(
+        "In today’s fiercely competitive business landscape, finding the right talent to lead your organisation is more"
+      ),
     },
   ];
+
   return (
     <div className="peo-container">
       {/* Main Section */}
       <div className="peo-main">
         <div className="peo-main-image">
-          <img
-            src="https://recruitindo.com/wp-content/uploads/2023/12/hire-in-indonesia-without-setting-up-a-company.png"
-            alt="Peo & Eor main image"
-          />
+          <img src={peoMainImage} alt="Peo & Eor main image" />
         </div>
         <div className="peo-main-contant">
-          <h2>{t('PEO & EOR')}</h2>
-          <h1>{t('Engage Workforce in India Without Company Registration')}</h1>
+          <h2>{t("PEO & EOR")}</h2>
+          <h1>{t("Engage Workforce in India Without Company Registration")}</h1>
           <p>
-            {t('Skip the complexities of launching your own business just to hire local talent. With our PEO solution, you can begin immediately!')}
+            {t(
+              "Skip the complexities of launching your own business just to hire local talent. With our PEO solution, you can begin immediately!"
+            )}
           </p>
-          <button className="peo-main-btn">{t('Let’s Talk')}</button>
+          <button className="peo-main-btn">{t("Let’s Talk")}</button>
         </div>
       </div>
 
       {/* Solution Section */}
       <div className="peo-solution">
-        <h2>{t('PEO & EOR')}</h2>
+        <h2>{t("PEO & EOR")}</h2>
         <h1>{t(`Advantages of Choosing Recruitindo's PEO/EOR Solutions`)}</h1>
         <div className="peo-solution-cards">
           {peoSolutions.map((data) => (
@@ -74,7 +91,7 @@ function PeoEor() {
 
       {/* Benefits Section */}
       <div className="peo-benefit">
-        <h2>{t('PEO & EOR')}</h2>
+        <h2>{t("PEO & EOR")}</h2>
         <h1>{t(`Benefits of Using Recruitindo's PEO / EOR Service`)}</h1>
         <div className="peo-benefit-cards">
           {peoBenefits.map((data) => (
@@ -90,7 +107,11 @@ function PeoEor() {
                   <div className="peo-benefit-unique">
                     <h2>{data.name}</h2>
                     <p>{data.desc}</p>
-                    <img src={rightArrow} alt="Right Arrow image" />
+                    <img
+                      onClick={handleclick}
+                      src={rightArrow}
+                      alt="Right Arrow image"
+                    />
                   </div>
                 </>
               )}
@@ -101,19 +122,19 @@ function PeoEor() {
 
       {/* Pricing PEO EOR */}
       <div className="peo-pricing">
-        <h2>{t('PRICING')}</h2>
-        <h1>{t('PEO / EOR Price Sheet')}</h1>
+        <h2>{t("PRICING")}</h2>
+        <h1>{t("PEO / EOR Price Sheet")}</h1>
         <div className="peo-pricing-cards">
           {peoPricing.map((data) => (
             <div key={data.id} className="peo-pricing-card">
               <h3>{data.name}</h3>
               <div className="peo-pricing-card-price">
-                <h5>{t('Starting from')}</h5>
+                <h5>{t("Starting from")}</h5>
                 <h3>{data.price}</h3>
-                <h5>{t('per employee per month')}</h5>
+                <h5>{t("per employee per month")}</h5>
               </div>
               <hr />
-              <h4>{t('Includes')}</h4>
+              <h4>{t("Includes")}</h4>
               <ul>
                 {data.include.map((item, index) => (
                   <li key={index}>{item}</li>
@@ -127,8 +148,8 @@ function PeoEor() {
 
       {/* Resources and Articles Section */}
       <div className="peo-resources">
-        <h2>{t('Resources')}</h2>
-        <h1>{t('Resources and Articles')}</h1>
+        <h2>{t("Resources")}</h2>
+        <h1>{t("Resources and Articles")}</h1>
         <div className="peo-resource-cards">
           {peoResources.map((ele, index) => (
             <>
@@ -159,7 +180,7 @@ function PeoEor() {
           ))}
         </div>
         <button className="peoResource-btn">
-          <a href="/resources">{t('See Our Resources')}</a>
+          <a href="/resources">{t("See Our Resources")}</a>
         </button>
       </div>
     </div>
